c31433fc34f61a975913e228e9a36085b7fda2d2
Sarvjeet Bhardwaj <sjitbh121993@gmail.com>
2022-07-08 18:27:31 +0530

    Creating project components

--- Housing-prediction/app.py
+++ Housing-prediction/app.py
@@ -5,6 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info('We are testing logger module')
     return "CI CD pipeline has been established."
 
 if __name__=="__main__":
